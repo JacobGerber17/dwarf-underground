94731ab8d69619c79062d7db60db7dc6c8845310
Jacob Gerber <jacogerb@iu.edu>
2018-06-11 11:33:02 -0400

    extract author component

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Comments from './Comments'
 import Share from './Share'
 import ArticleBody from './ArticleBody';
 import OtherArticles from './OtherArticles';
+import Author from './Author';
 
 class App extends Component {
   render() {
@@ -17,13 +18,7 @@ class App extends Component {
         <main className="expanded row">
           <div className="large-8 medium-12 columns article">
             <h2 className="article-title">Gold Madness - Fact or Fiction?</h2>
-            <div className="avatar">
-              <img src="http://www.zbrushcentral.com/attachment.php?attachmentid=376082" alt="author" />
-              <div className="author-info">
-                <p className="author-name">By Domri, son of Flug</p>
-                <p className="date">on 28 April, 3018 of the Third Age</p>
-              </div>
-            </div>
+            <Author />
             <ArticleBody />
             <div className="article-links">
               <Comments />
